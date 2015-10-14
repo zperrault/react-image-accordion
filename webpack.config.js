a93f31d73dd0eb26dc6771c29996959ab544bff2
Zach Perrault <zach.perrault@gmail.com>
2015-10-13 20:05:40 -0400

    Working on project setup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    library: 'ImageAccordion',
+    libraryTarget: 'umd'
+  },
+
+  debug: process.env.NODE_ENV !== 'production',
+
+  devtool: 'sourcemap',
+
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs: 'react',
+        commonjs2: 'react',
+        amd: 'react'
+      }
+    }
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader?stage=0',
+        include: [
+          path.resolve(__dirname, 'src')
+        ]
+      }
+    ]
+  },
+}
